359c3533e5dc0dfb0787ac6bcb23ab3196347ecc
miaprica <73109835+miaprica@users.noreply.github.com>
2021-01-19 01:57:00 +0100

    adding the score value display and UI text

--- 2DCarGame/Assets/Scripts/ScoreDisplay.cs
+++ 2DCarGame/Assets/Scripts/ScoreDisplay.cs
@@ -8,16 +8,16 @@ public class ScoreDisplay : MonoBehaviour
     Text scoreText;
     GameSession gameSession;
 
-    //Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        scoreText.GetComponent<Text>();
+        scoreText = GetComponent<Text>();
         gameSession = FindObjectOfType<GameSession>();
     }
 
-    //Update is called once per frame
-    public void Update()
+    // Update is called once per frame
+    void Update()
     {
-        scoreText.text = gameSession.GetScore().ToString();        //i was getting a null reference error, in update the error is updating so i commented it out, its easier for the eye to see 1 error than "Error: 999+"
+        scoreText.text = gameSession.GetScore().ToString();
     }
 }
